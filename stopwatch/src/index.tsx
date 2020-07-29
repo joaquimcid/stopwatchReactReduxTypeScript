592b0ca865ad6c7d7ab379bd22d462c2eed06ded
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-29 18:01:12 +0200

    WIP

--- stopwatch/src/index.tsx
+++ stopwatch/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducerStopWatch } from './redux/reducers/StopWatchReducer';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var store = createStore(() => reducerStopWatch());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
