87adcd28bbe7819766e83fa97729c20f898ef58a
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-30 16:34:44 +0200

    WIP

--- stopwatch/src/components/ElapsedTimeDisplay.tsx
+++ stopwatch/src/components/ElapsedTimeDisplay.tsx
@@ -1,25 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import Display from'./Display';
 import FormatMilliSeconds from '../converter/FormatMilliSeconds';
-// import { useSelector } from 'react-redux';
-// import { startedTimeSelector } from '../redux/selectors/startedTimeSelector';
-// import { pausedTimeSelector } from '../redux/selectors/pausedTimeSelector';
 import log, { ComponentsEnum } from './LogDebug';
 import { StopWatchStatusEnum } from '../redux/reducers/StopWatchStatusEnum';
 import IHavePausedTime from '../redux/state/IHavePausedTime';
 import IHaveStatus from '../redux/state/IHaveStatus';
 import IHaveStartedTime from '../redux/state/IHaveStartedTime';
-// import { statusSelector } from '../redux/selectors/statusSelector';
-// import { StopWatchStatusEnum } from '../redux/reducers/StopWatchStatusEnum';
-
-function timeToShow( whenStartedTime: number, whenPausedTime: number|undefined)
-{
-  if (!whenStartedTime) return 0;
-
-  if (!whenPausedTime || whenPausedTime !== null) return Date.now() - whenStartedTime;
-  
-  return whenPausedTime!-whenStartedTime;
-}
 
 interface ElapsedTimeDisplayProps extends 
   IHaveStatus,
@@ -31,28 +17,30 @@ interface ElapsedTimeDisplayProps extends
 // is not stopping, we should subscribe when there are state change or paused/start times
 export default function ElapsedTimeDisplay(props:ElapsedTimeDisplayProps)
 { 
-  // const startedTime = useSelector(startedTimeSelector);
-  // const pausedTime = useSelector(pausedTimeSelector);
-  const timeToShowInMilliSeconds = timeToShow(props.startedTime, props.pausedTime);
-  const valueToDisplayPretty = FormatMilliSeconds(timeToShowInMilliSeconds);
-  const [value, setValue] = useState(valueToDisplayPretty);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
     const request = requestAnimationFrame(() => {
-     if (props.status !== StopWatchStatusEnum.PAUSED) {
-        log(ComponentsEnum.ElapsedTimeDisplay, `elapsedTimeDisplay ${valueToDisplayPretty}. timeToShowInMilliSeconds: ${timeToShowInMilliSeconds}, startedTime: ${props.startedTime}, pausedTime: ${props.pausedTime}`);
-        setValue(FormatMilliSeconds(timeToShow(props.startedTime, props.pausedTime)));          
-      } else
+      
+      if (props.status === StopWatchStatusEnum.PAUSED) {
+        log(ComponentsEnum.ElapsedTimeDisplay, ` Status: ${props.status}, Value: ${props.pausedTime! - props.startedTime}`);
+        setValue(props.pausedTime! - props.startedTime);          
+      } else if(props.status === StopWatchStatusEnum.STARTED)
       {
-        log(ComponentsEnum.ElapsedTimeDisplay, 'UseEffect skip')
+        log(ComponentsEnum.ElapsedTimeDisplay, ` Status: ${props.status}, Value: ${Date.now() - props.startedTime}`);
+        setValue(Date.now() - props.startedTime);
+      } else {
+        log(ComponentsEnum.ElapsedTimeDisplay, ` Status: ${props.status}, Value: ${0}`);
+        setValue(0);
       }
+      
     });
     return () => cancelAnimationFrame(request);
-  }, [timeToShow(props.startedTime, props.pausedTime)]);
+  });
   
   return (
     <div className="elapsedTime">
-      <Display value={value} />
+      <Display value={FormatMilliSeconds(value)} />
     </div>
   );
 }
