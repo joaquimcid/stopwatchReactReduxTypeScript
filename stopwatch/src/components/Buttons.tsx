807ea4061c94f0f16f9ee5c2dad6aaa828a857ad
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-30 11:24:17 +0200

    Dispatch UserAction

--- stopwatch/src/components/Buttons.tsx
+++ stopwatch/src/components/Buttons.tsx
@@ -2,50 +2,63 @@ import React from 'react';
 import { StopWatchStatusEnum } from '../redux/reducers/StopWatchStatusEnum';
 import { useSelector } from 'react-redux';
 import { statusSelector } from '../redux/selectors/statusSelector';
-
-function onButtonClick(action:string) {
-  //console.log(action);
-  // const dispatch = useDispatch();
-  // () => dispatch(PauseAction);
-}
+import { useDispatch } from 'react-redux'
+import { StartAction, NewLapAction, ResetAction, ContinueAction, PauseAction } from '../redux/actions/UserAction';
+import UserAction from '../redux/actions/UserAction';
 
 
-function newLapOrResetClicked (status:StopWatchStatusEnum) {
-  if (status === StopWatchStatusEnum.PAUSED) {
-    console.log('LAP_RESET_BTN: PAUSED -> RESET');
-    return onButtonClick('RESET');
-  }
 
-  if(status === StopWatchStatusEnum.STARTED) {
-    console.log('LAP_RESET_BTN: STARTED -> NEWLAP');
-    return onButtonClick('NEWLAP');
-  }
-};
 
-function startOrPauseClicked(status:StopWatchStatusEnum) {
- 
-  if(status === StopWatchStatusEnum.INITIAL)
-  {
-    console.log('START_PAUSE_BTN: INITIAL -> START');
-    return onButtonClick('START');
-  }
 
-  if (status === StopWatchStatusEnum.PAUSED)
-  {
-    console.log('START_PAUSE_BTN: PAUSED -> CONTINUE');
-    return onButtonClick('CONTINUE');
-  }
 
-  if(status === StopWatchStatusEnum.STARTED)
-  {
-    console.log('START_PAUSE_BTN: STARTED -> PAUSE');
-    return onButtonClick('PAUSE');
-  }
-};
 
 export default function Buttons() {
   const status = useSelector(statusSelector);
+  const dispatch = useDispatch();
+
+  function onButtonClick(action:UserAction) {
+    // {() => dispatch({ type: action })}
+  
+    console.log("onButtonClick, action to dispatch: " + action);
+    dispatch(action);
+    //console.log(action);
+    // const dispatch = useDispatch();
+    // () => dispatch(PauseAction);
+  }
+
   /* status => INITIAL, STARTED, PAUSED */
+  function newLapOrResetClicked (status:StopWatchStatusEnum) {
+    if (status === StopWatchStatusEnum.PAUSED) {
+      console.log('LAP_RESET_BTN: PAUSED -> RESET');
+      return onButtonClick(ResetAction);
+    }
+  
+    if(status === StopWatchStatusEnum.STARTED) {
+      console.log('LAP_RESET_BTN: STARTED -> NEWLAP');
+      return onButtonClick(NewLapAction);
+    }
+  };
+  
+  function startOrPauseClicked(status:StopWatchStatusEnum) {
+   
+    if(status === StopWatchStatusEnum.INITIAL)
+    {
+      console.log('START_PAUSE_BTN: INITIAL -> START');
+      return onButtonClick(StartAction);
+    }
+  
+    if (status === StopWatchStatusEnum.PAUSED)
+    {
+      console.log('START_PAUSE_BTN: PAUSED -> CONTINUE');
+      return onButtonClick(ContinueAction);
+    }
+  
+    if(status === StopWatchStatusEnum.STARTED)
+    {
+      console.log('START_PAUSE_BTN: STARTED -> PAUSE');
+      return onButtonClick(PauseAction);
+    }
+  };
 
   return (
     <div id="buttons" className ="buttons">
--- stopwatch/src/redux/reducers/StopWatchReducer.tsx
+++ stopwatch/src/redux/reducers/StopWatchReducer.tsx
@@ -1,20 +1,66 @@
 
 import {initialState, stopWatchState } from '../state/StopWatchState';
-import UserAction, { StartAction } from '../actions/UserAction';
-// import UserAction, { StartAction, ContinueAction, NewLapAction, PauseAction, ResetAction } from '../actions/UserAction';
+import UserAction, 
+{ StartAction, 
+  PauseAction, 
+  ContinueAction, 
+  NewLapAction, 
+  ResetAction } from '../actions/UserAction';
 import { StopWatchStatusEnum } from './StopWatchStatusEnum';
 
 export function reducerStopWatch(state = initialState, action: UserAction): stopWatchState {
+  // console.log('Reducer received action ' + action);
+  // console.log('Reducer received action type ' + action.type);
+
+  // if (!state) return initialState;
+  // if (!action) return initialState;
+
   switch (action) {
     case StartAction: {
       return {
-                  status: StopWatchStatusEnum.STARTED,
-                  startedTime: Date.now(),
-                  // pausedTime: null,
-                  laps: [],
-                  sumOfLaps: state.sumOfLaps
-                };
+        status: StopWatchStatusEnum.STARTED,
+        startedTime: Date.now(),
+        laps: [],
+        sumOfLaps: state.sumOfLaps
+      };
     }
+    case PauseAction:
+      return {
+          status: StopWatchStatusEnum.PAUSED,      
+          startedTime: state.startedTime,
+          pausedTime: Date.now(),
+          laps: state.laps,
+          sumOfLaps: state.sumOfLaps
+        };
+        
+    case ContinueAction:
+      let updateStartedTime = state.startedTime + Date.now() - (state.pausedTime || 0);
+      return {
+          status: StopWatchStatusEnum.STARTED,
+          startedTime: updateStartedTime,
+          laps: state.laps,
+          sumOfLaps: state.sumOfLaps
+        };
+
+    case NewLapAction:
+      const currentLapTime = Date.now()-state.startedTime-state.sumOfLaps;
+
+      const newLap = {
+        index: state.laps.length+1,
+        totalTime: currentLapTime,
+        isMin: false,
+        isMax: false,
+      } 
+
+      return {
+          status: StopWatchStatusEnum.STARTED,
+          startedTime: state.startedTime,
+          laps: state.laps.concat(newLap),
+          sumOfLaps: state.sumOfLaps+currentLapTime
+        };
+
+    case ResetAction:
+      return initialState;
     default:
       return state
   }
