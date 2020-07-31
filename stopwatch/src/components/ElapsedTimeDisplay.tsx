9c691ce2fa9148499542a286e9c8f3076ba37d6b
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-31 11:52:48 +0200

    Improve useEffect to avoid not needed rerenders

--- stopwatch/src/components/ElapsedTimeDisplay.tsx
+++ stopwatch/src/components/ElapsedTimeDisplay.tsx
@@ -5,11 +5,6 @@ import IHavePausedTime from '../redux/state/IHavePausedTime';
 import IHaveStartedTime from '../redux/state/IHaveStartedTime';
 import log, { ComponentsEnum } from './LogDebug';
 
-function isStoppedOrPausedOrRunning(started:number, paused:number|undefined):[number, string] { 
-  return started === 0 ?  [0, 'INITIAL'] 
-                       : ( !!paused ?  [paused! - started, 'PAUSED'] 
-                                    :  [Date.now() - started, 'STARTED']);
-}
 
 interface ElapsedTimeDisplayProps extends 
   IHaveStartedTime,
@@ -19,15 +14,24 @@ interface ElapsedTimeDisplayProps extends
 export default function ElapsedTimeDisplay(props:ElapsedTimeDisplayProps)
 { 
   const [value, setValue] = useState(0);
-  
   useEffect(() => {
-    const request = requestAnimationFrame(() => {
-      let currentValueState:[number, string] = isStoppedOrPausedOrRunning(props.startedTime, props.pausedTime);
-      log(ComponentsEnum.ElapsedTimeDisplay, `useEffect status: ${currentValueState[1]} value ${currentValueState[0]}`)
-      setValue(currentValueState[0]);
-    });
-    return () => cancelAnimationFrame(request);
-  });
+    let lastRequestAnimationFrame:number = -1;
+
+    function performUpdate(started:number, paused?:number):void {
+      let newValue:[number, string] = started === 0 ?  [0, 'INITIAL'] 
+                       : ( !!paused ?  [paused! - started, 'PAUSED'] 
+                                    :  [Date.now() - started, 'STARTED']);
+     
+     log(ComponentsEnum.ElapsedTimeDisplay, `useEffect status: ${newValue[1]} value ${newValue[0]}`)
+
+      setValue(newValue[0]);
+      lastRequestAnimationFrame = requestAnimationFrame(() => performUpdate(props.startedTime, props.pausedTime));
+    }
+    lastRequestAnimationFrame = requestAnimationFrame(() => performUpdate(props.startedTime, props.pausedTime));
+    return () => {
+      if (lastRequestAnimationFrame !== -1) return cancelAnimationFrame(lastRequestAnimationFrame);
+    };
+  }, [props.startedTime, props.pausedTime]);
   
   return (
     <div className="elapsedTime">
