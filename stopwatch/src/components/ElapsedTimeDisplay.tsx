0a6c86f94d4f8963a3f6613abfbc4efdc98f8289
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-31 13:42:49 +0200

    Works better, there are no infinite loop when stopwatch is stopped

--- stopwatch/src/components/ElapsedTimeDisplay.tsx
+++ stopwatch/src/components/ElapsedTimeDisplay.tsx
@@ -15,23 +15,23 @@ export default function ElapsedTimeDisplay(props:ElapsedTimeDisplayProps)
 { 
   const [value, setValue] = useState(0);
   useEffect(() => {
-    let lastRequestAnimationFrame:number = -1;
+    let lastRequest:number = -1;
 
     function performUpdate(started:number, paused?:number):void {
       let newValue:[number, string] = started === 0 ?  [0, 'INITIAL'] 
                        : ( !!paused ?  [paused! - started, 'PAUSED'] 
                                     :  [Date.now() - started, 'STARTED']);
+      
+      log(ComponentsEnum.ElapsedTimeDisplay, `useEffect status: ${newValue[1]} value ${value} newValue ${newValue[0]} `)
      
-     log(ComponentsEnum.ElapsedTimeDisplay, `useEffect status: ${newValue[1]} value ${newValue[0]}`)
-
+      // if (value !== newValue[0]) 
       setValue(newValue[0]);
-      lastRequestAnimationFrame = requestAnimationFrame(() => performUpdate(props.startedTime, props.pausedTime));
     }
-    lastRequestAnimationFrame = requestAnimationFrame(() => performUpdate(props.startedTime, props.pausedTime));
-    return () => {
-      if (lastRequestAnimationFrame !== -1) return cancelAnimationFrame(lastRequestAnimationFrame);
-    };
-  }, [props.startedTime, props.pausedTime]);
+    
+    lastRequest = requestAnimationFrame(() => performUpdate(props.startedTime, props.pausedTime));
+    
+    return () => { if (lastRequest !== -1) return cancelAnimationFrame(lastRequest);};
+  }, [props.startedTime, props.pausedTime, value]);
   
   return (
     <div className="elapsedTime">
