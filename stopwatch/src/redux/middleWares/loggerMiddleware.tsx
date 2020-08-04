b7498d3062b8e3f29ff471586812fc76512ceee7
Joaquin Cid <joaquin@weareadaptive.com>
2020-08-04 14:06:06 +0200

    WIP

--- stopwatch/src/redux/middleWares/loggerMiddleware.tsx
+++ stopwatch/src/redux/middleWares/loggerMiddleware.tsx
@@ -1,17 +1,15 @@
 import { Dispatch, Middleware, MiddlewareAPI } from 'redux';
 import { IStopWatchAction } from '../actions/StopWatchAction';
-import log, { ComponentsEnum, logGroup, logGroupEnd } from '../../components/LogDebug';
+import log, { ComponentsEnum } from '../../components/LogDebug';
 
 // TODO Understand this line
 export const loggerMiddleware: Middleware = (api: MiddlewareAPI) => (next: Dispatch<IStopWatchAction>) => (action:IStopWatchAction) => {
-  logGroup(ComponentsEnum.LoggerMiddleWare, action.type);
 
   log(ComponentsEnum.LoggerMiddleWare, `state before dispatch ${action.type} action`, api.getState());
 
   const actionValue = next(action);
 
   log(ComponentsEnum.LoggerMiddleWare, `state after dispatch ${action.type} action`, api.getState());
-  logGroupEnd(ComponentsEnum.LoggerMiddleWare);
 
   return actionValue;
 };
--- stopwatch/src/redux/reducers/StopWatch/StopWatchReducer.tsx
+++ stopwatch/src/redux/reducers/StopWatch/StopWatchReducer.tsx
@@ -3,8 +3,11 @@ import {IStopWatchAction, StopWatchActionType} from '../../actions/StopWatchActi
 import { StopWatchStatusEnum } from './StopWatchStatusEnum';
 import log, { ComponentsEnum } from '../../../components/LogDebug';
 import { lapsList, emptyLapsList } from '../../state/StopWatch/ILapsState';
+import { logGroup, logGroupEnd } from '../../../components/LogDebug';
 
 function isAllowedThisStateChange(state:StopWatchStatusEnum, actionType: StopWatchActionType):boolean {
+
+  if (actionType.startsWith('@@redux/INIT') || actionType.startsWith('@@redux/PROBE_UNKNOWN')) return false;
 
   if(state === StopWatchStatusEnum.INITIAL
     && actionType === StopWatchActionType.START) return true;
@@ -26,64 +29,76 @@ function isAllowedThisStateChange(state:StopWatchStatusEnum, actionType: StopWat
     && actionType === StopWatchActionType.CONTINUE) return true;
   //     log(ComponentsEnum.Buttons, 'START_PAUSE_BTN: PAUSED -> Dispatch(CONTINUE)');
 
-  log(ComponentsEnum.StopWatchReducer, `When status [${state}] is dispatched the NOT ALLOWED action[${actionType.toString()}]`);
+  log(ComponentsEnum.StopWatchReducer, `Not allowed transition [${actionType.toString()}] when state is [${state}] `);
   return false;
 }
 
 export default function StopWatchReducer(state = initialState, action: IStopWatchAction): stopWatchState {
-  log(ComponentsEnum.StopWatchReducer, 'Reducer received action ' + action);
-  log(ComponentsEnum.StopWatchReducer, 'Reducer received action type ' + action.type);
+  logGroup(ComponentsEnum.StopWatchReducer, `Reducer received action ${action.type}`);
+  log(ComponentsEnum.StopWatchReducer, state);
+  // if (!action || action.type.startsWith('@@redux/INIT')) return initialState;
+  let result:stopWatchState = state;
 
-  if (!action || action.type.startsWith('@@redux/INIT')) return initialState;
+  if (isAllowedThisStateChange(state.status, action.type)) 
+  {
+    switch (action.type) {
+      case StopWatchActionType.START: {
+        result = {
+          status: StopWatchStatusEnum.STARTED,
+          startedTime: Date.now(),
+          laps: emptyLapsList,
+        };
+        break;
+      }
+      case StopWatchActionType.PAUSE:
+        result = {
+            status: StopWatchStatusEnum.PAUSED,      
+            startedTime: state.startedTime,
+            pausedTime: Date.now(),
+            laps: state.laps,
+          };
+        break;
+      case StopWatchActionType.CONTINUE:
+        let updateStartedTime = state.startedTime + Date.now() - (state.pausedTime || 0);
+        result = {
+            status: StopWatchStatusEnum.STARTED,
+            startedTime: updateStartedTime,
+            laps: state.laps,
+          };
+        break;
+      case StopWatchActionType.NEWLAP:
+        result = NewLap(state);
+        break;
 
-  if (!isAllowedThisStateChange(state.status, action.type)) return state;
-
-  switch (action.type) {
-    case StopWatchActionType.START: {
-      return {
-        status: StopWatchStatusEnum.STARTED,
-        startedTime: Date.now(),
-        laps: emptyLapsList,
-      };
+      case StopWatchActionType.RESET:
+        result = initialState;
+        break;
+      default:
+        break;
+        //return state
     }
-    case StopWatchActionType.PAUSE:
-      return {
-          status: StopWatchStatusEnum.PAUSED,      
-          startedTime: state.startedTime,
-          pausedTime: Date.now(),
-          laps: state.laps,
-        };
-        
-    case StopWatchActionType.CONTINUE:
-      let updateStartedTime = state.startedTime + Date.now() - (state.pausedTime || 0);
-      return {
-          status: StopWatchStatusEnum.STARTED,
-          startedTime: updateStartedTime,
-          laps: state.laps,
-        };
-
-    case StopWatchActionType.NEWLAP:
-      const currentLapTime = Date.now()-state.startedTime-state.laps.sumOfLaps;
-      const newLap = {
-        index: state.laps.records.length+1,
-        totalTime: currentLapTime,
-      }
-      const newRecords = state.laps.records.concat(newLap);
-      const newLapsList:lapsList = {
-        records:newRecords, 
-        sumOfLaps:state.laps.sumOfLaps+currentLapTime, 
-        minValueIndex:newRecords.reduce((iMin, x, i, arr) => x.totalTime < arr[iMin].totalTime ? i : iMin, 0), 
-        maxValueIndex:newRecords.reduce((iMin, x, i, arr) => x.totalTime > arr[iMin].totalTime ? i : iMin, 0)
-      }
-      return {
-          status: StopWatchStatusEnum.STARTED,
-          startedTime: state.startedTime,
-          laps: newLapsList,
-        };
-
-    case StopWatchActionType.RESET:
-      return initialState;
-    default:
-      return state
   }
+  logGroupEnd(ComponentsEnum.StopWatchReducer);
+  return result;
+}
+
+function NewLap(state: stopWatchState) : stopWatchState {
+  const currentLapTime = Date.now() - state.startedTime - state.laps.sumOfLaps;
+  const newLap = {
+    index: state.laps.records.length + 1,
+    totalTime: currentLapTime,
+  };
+  const newRecords = state.laps.records.concat(newLap);
+  const newLapsList: lapsList = {
+    records: newRecords,
+    sumOfLaps: state.laps.sumOfLaps + currentLapTime,
+    minValueIndex: newRecords.reduce((iMin, x, i, arr) => x.totalTime < arr[iMin].totalTime ? i : iMin, 0),
+    maxValueIndex: newRecords.reduce((iMin, x, i, arr) => x.totalTime > arr[iMin].totalTime ? i : iMin, 0)
+  };
+  
+  return {
+    status: StopWatchStatusEnum.STARTED,
+    startedTime: state.startedTime,
+    laps: newLapsList,
+  };
 }
